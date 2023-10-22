585bb08b62988890e87aedaf5633da5f41204b94
Dyabolo-Light <seagulljhonl@gmail.com>
2023-10-23 00:52:46 +0300

    Файл с фильтрами

---
+++ backend/api/filters.py
@@ -0,0 +1,33 @@
+from django_filters.rest_framework import FilterSet, filters
+from rest_framework.filters import SearchFilter
+
+from recipes.models import Recipe, Tag
+
+
+class IngredientSearchFilter(SearchFilter):
+    search_param = 'name'
+
+
+class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value:
+            return queryset.filter(favourite__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
